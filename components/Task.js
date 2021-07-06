191545012300ed9487762be32abd5d2e83376fe3
Juan Ibarra <jmibarra86@gmail.com>
2021-07-06 12:01:12 -0300

    Commit inicial con archivos iniciales de la app y primeros 2 templates de pruebas

---
+++ components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = () => {
+    return(
+        <View>
+            <Text> Tarea de prueba</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
