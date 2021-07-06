03187f8c38f1822469376cd4109ca1ecfa68d837
Juan Ibarra <jmibarra86@gmail.com>
2021-07-06 13:39:51 -0300

    Se agregan los botones para poder escribir y agregar tareas, se comienza a preparar el uso de estados para guardar las tareas

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task,setTask] = useState();
+
+  //const handleAddTask
+
   return (
     <View style={styles.container}>
 
@@ -20,7 +24,21 @@ export default function App() {
 
       </View>
       
-    </View>
+    
+
+      {/* Escribir las tareas */}
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTaskWrapper} >
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+      
+
+        <TouchableOpacity> 
+          <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </View> 
   );
 }
 
@@ -39,5 +57,34 @@ const styles = StyleSheet.create({
   },
   items:{
     marginTop: 30
-  }
+  },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal:15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  addText: {},
 });
